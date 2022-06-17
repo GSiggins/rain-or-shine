f496df2bc7b6f25098b0068948d290c9ce7ca789
GSiggins <gregory.siggins@gmail.com>
2022-06-16 18:29:37 -0700

    restructured HTML to prepare for card population

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,41 +1,13 @@
 const APIkey = 'fddd66118ec569d3e86667207b7ec986'
 const searchBtn = document.querySelector('#search-btn')
 let searchBarInput;
-// fetch weather url = https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-// geocoding fetch url = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-// const weatherAPI = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'fddd66118ec569d3e86667207b7ec986',
-//         'X-RapidAPI-Host': 'https://api.openweathermap.org/data/3.0/onecall'
-//     }
-// };
-
-// const geoCodeAPI = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'fddd66118ec569d3e86667207b7ec986',
-//         'X-RapidAPI-Host': 'http://api.openweathermap.org/geo/1.0/direct'
-//     }
-// };
+const cardArray = [document.querySelector('.card')]
 
 function searchClick(event) {
     event.preventDefault();
     searchBarInput = document.querySelector('#search-text').value;
     console.log(searchBarInput)
-    // build first url request
-    // var weatherString = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${APIkey}`;
-
-    // // build second url request
-    // var geoCodeString = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${state$code},${countrycode}&limit=${limit}&appid=${APIkey}`;
- 
-    // Call fetch functions
     geoCodeQuery();
-    // weatherQuery();
-    //window.location.href = 'results-page.html';
-    // return variables to use in fetch functions
     return;
 }
 
@@ -69,6 +41,14 @@ function weatherQuery(data) {
         })
 }
 
-
+function cardPop(dayInfoArray) {
+    for (let i = 0; i < cardArray.length; i++) {
+        let temp = dayInfoArray[i].temp.day;
+        let wind = dayInfoArray[i].weather.wind_speed;
+        let humidity = dayInfoArray[i].humidity;
+        let uvi = dayInfoArray[i].uvi;
+        
+    }
+}
 
 searchBtn.addEventListener('click', searchClick)
