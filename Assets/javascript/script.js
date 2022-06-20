541c4e5c72fad6c572bcfd1e6dd313dc1072055c
GSiggins <gregory.siggins@gmail.com>
2022-06-20 10:17:54 -0700

    modified population of date/time on each card

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -3,7 +3,9 @@ const searchBtn = document.querySelector('#search-btn')
 let searchBarInput;
 const cards = document.querySelectorAll('.card')
 const cardArray = [...cards]
-console.log(cardArray)
+const dayText = document.querySelectorAll('.card-text')
+const dayTextArray = [...dayText]
+console.log(dayTextArray)
 
 function searchClick(event) {
     event.preventDefault();
@@ -49,15 +51,21 @@ function weatherQuery(data) {
 
 
 function cardPop(weatherInfoObj) {
+    // FOR loop that assigns variables to the object properties we want
+    // to populate within the cards. 
+
+    //
     for (let i = 0; i < 5; i++) {
         let temp = weatherInfoObj.daily[i].temp.day;
         let wind = JSON.stringify(weatherInfoObj.daily[i].wind_speed);
         let humidity = weatherInfoObj.daily[i].humidity;
         let uvi = weatherInfoObj.daily[i].uvi;
+
         let cardTemp = $('.card-temp').eq(i);
         let cardWind = $('.card-wind').eq(i);
         let cardHumidity = $('.card-humidity').eq(i);
         let cardUVI = $('.card-uvi').eq(i);
+
         console.log(cardTemp, cardWind, cardHumidity, cardUVI)
         cardTemp.text('Tempurature: ' + temp);
         cardWind.text('Wind: ' + wind);
@@ -67,8 +75,7 @@ function cardPop(weatherInfoObj) {
 }
 
 timeclock = function () {
-    var time = moment().format('[It is currently ] dddd, MMMM Do YYYY [ at ] hh:mm:ss a');
-    console.log(time)
+    var time = moment().format('[It is currently ] dddd, MMMM Do, YYYY [ at ] hh:mm:ss a');
     document.getElementById('current-day').textContent = time;
 }
 
@@ -82,7 +89,28 @@ localStorage.setItem('userSearch', JSON.stringify(newInput));
 
 searchBtn.addEventListener('click', searchClick)
 
+function daySet(dayTextArray) {
 
+    var today = document.getElementById('day1');
+    var tomorrow = document.getElementById('day2');
+    var thirdDay = document.getElementById('day3');
+    var fourthDay = document.getElementById('day4');
+    var fifthDay = document.getElementById('day5');
 
+    today.textContent = moment().format("MM-DD");
+    tomorrow.textContent = moment().add(1, 'days').format("MM-DD");
+    thirdDay.textContent = moment().add(2, 'days').format("MM-DD");
+    fourthDay.textContent = moment().add(3, 'days').format("MM-DD");
+    fifthDay.textContent = moment().add(4, 'days').format("MM-DD");
+
+    for (let j = 0; j < 5; j++) {
+        let dayTextDisplay = $('.card-text').eq(j);
+        let dateText = moment().add(0+[j], "d").format('dddd');
+        console.log(moment().add(0+[j], "d").format('dddd'));
+        dayTextDisplay.text("This is the forecast for " + dateText);
+    }
+}
+
+daySet()
 timeclock()
 setInterval(timeclock, 1000)
